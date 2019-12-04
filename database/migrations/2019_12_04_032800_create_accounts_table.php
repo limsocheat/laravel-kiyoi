c7dbcb26547c73cc054b055ef2a117933b2298dc
z3r0 <z3r0@localhost.localdomain>
2019-12-04 11:15:21 +0700

    Added Account table and Seeder

---
+++ database/migrations/2019_12_04_032800_create_accounts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('code');
+            $table->text('description')->nullable();
+            $table->boolean('active')->default(1);
+            $table->double('debit');
+            $table->double('balance');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
