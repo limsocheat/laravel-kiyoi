05b96af7ea194a7bcb97ddd10ca93f7e4be96d77
z3r0 <z3r0@localhost.localdomain>
2019-12-10 16:09:32 +0700

    Add relationship between Supplier and Purchase

--- app/Supplier.php
+++ app/Supplier.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    //
+    public function purchases()
+    {
+    	return $this->hasMany(\App\Purchase::class);
+    }
 }
