4ed91556c9b633392b7805ec99f0c9e516d9ad24
z3r0 <z3r0@localhost.localdomain>
2019-12-11 11:56:49 +0700

    Add ExpenseResource

---
+++ app/Http/Resources/ExpenseResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ExpenseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'date' => $this->date,
+            'description' => $this->description,
+            'active' => $this->active,
+            'amount' => $this->amount,
+            'expense_for' => $this->expense_for,
+            'created_at' => $this->created_at->toDateTimeString(),
+            'expense_category' => $this->expense_category,
+        ];
+    }
+}
