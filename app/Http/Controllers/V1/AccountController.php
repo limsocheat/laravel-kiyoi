b52ade343799f2c291d9c57abc872e0f73a37394
z3r0 <z3r0@localhost.localdomain>
2019-12-17 09:17:52 +0700

    Recorrect AccountController

--- app/Http/Controllers/V1/AccountController.php
+++ app/Http/Controllers/V1/AccountController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers\V1;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -17,21 +16,10 @@ class AccountController extends Controller
     public function index()
     {   
         $itemsPerPage = empty(request('itemsPerPage')) ? 5 : (int)request('itemsPerPage');
-<<<<<<< HEAD
         $account = Account::with('payrolls')->orderBy('id', 'desc')->get();
-
-
         // return $account;
         return response()->json(['account' => $account]);
-=======
-        $account = Account::with('payrolls')->get();
-
-
-        return $account;
-        // return response()->json(['account' => $account]);
->>>>>>> af119676611ad7ae8a5874a995beec90899a8df8
     }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -43,29 +31,18 @@ class AccountController extends Controller
         $request->validate([
             'name' => 'required',
             'code' => 'required',
-<<<<<<< HEAD
-=======
-            'debit' => 'required',
->>>>>>> af119676611ad7ae8a5874a995beec90899a8df8
             'balance' => 'required',
         ]);
-
         $account = new Account();
         $account->user_id = auth()->user()->id;
         $account->code = $request->code;
         $account->name = $request->name;
-<<<<<<< HEAD
         // $account->debit = $request->debit;
-=======
-        $account->debit = $request->debit;
->>>>>>> af119676611ad7ae8a5874a995beec90899a8df8
         $account->description = $request->description;
         $account->balance = $request->balance;
         $account->save();
-
         return response()->json(['created' => true]);
     }
-
     /**
      * Display the specified resource.
      *
@@ -76,7 +53,6 @@ class AccountController extends Controller
     {
         //
     }
-
     /**
      * Update the specified resource in storage.
      *
@@ -89,28 +65,17 @@ class AccountController extends Controller
         $request->validate([
             'name' => 'required',
             'code' => 'required',
-<<<<<<< HEAD
-=======
-            'debit' => 'required',
->>>>>>> af119676611ad7ae8a5874a995beec90899a8df8
             'balance' => 'required',
         ]);
-
         $account = Account::findOrFail($id);
         $account->user_id = auth()->user()->id;
         $account->code = $request->code;
         $account->name = $request->name;
-<<<<<<< HEAD
-=======
-        $account->debit = $request->debit;
->>>>>>> af119676611ad7ae8a5874a995beec90899a8df8
         $account->description = $request->description;
         $account->balance = $request->balance;
         $account->save();
-
         return response()->json(['updated' => true]);
     }
-
     /**
      * Remove the specified resource from storage.
      *
@@ -121,7 +86,6 @@ class AccountController extends Controller
     {
         $account = Account::findOrFail($id);
         $account->delete();
-
         return response()->json(['deleted' => true]);
     }
 }
