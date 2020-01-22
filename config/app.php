f9782f780a8fa1aff6bf4be98f5ce7d219434549
z3r0 <z3r0@localhost.localdomain>
2020-01-22 15:30:22 +0700

    Added ImageIntervention Package

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
          */
         Maatwebsite\Excel\ExcelServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
-
+        Intervention\Image\ImageServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -227,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Image' => Intervention\Image\Facades\Image::class
 
     ],
 
