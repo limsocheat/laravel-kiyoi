ec506bac9a87d06f591945e431cf4e5ef3a4f4f3
N0SYS73M1SS4F3 <cambokid75@gmail.com>
2020-03-20 15:47:55 +0700

    Appends Attribute "created_sale"

--- app/Sale.php
+++ app/Sale.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Activitylog\Contracts\Activity;
 
+use Carbon\Carbon;
+
 class Sale extends Model
 {
 
@@ -18,14 +20,19 @@ class Sale extends Model
 		'description', 'active', 'payment_status', 'total', 'paid', 'due', 'member_id', 'user_id', 'branch_id',
 	];
 
-    protected $appends = ['grand_total', 'due_amount', 'total_quantity', 'total_price', 'total_discount', 'sub_total', 'payment_status', 'user_name'];
+    protected $appends = ['created_sale', 'grand_total', 'due_amount', 'total_quantity', 'total_price', 'total_discount', 'sub_total', 'payment_status', 'user_name'];
 
     
     public function tapActivity(Activity $activity, string $eventName)
     {
         $activity->description = "Sale was {$eventName}";
     }
-    
+
+    public function getCreatedSaleAttribute()
+    {
+        return Carbon::parse($this->created_at)->toDateString();
+    }
+
     public function getUserNameAttribute()
     {
         # code...
